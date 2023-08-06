d7dcc3926dfab2f118732bf432d3a41b35c4814f
Sergii Dudkin <sergii.dudkin@gmail.com>
2023-08-06 18:01:26 +0300

    Complete textTermBuilder

--- ChemParser.js
+++ ChemParser.js
@@ -12,6 +12,7 @@ function buildPrefixTree(prefix_tree, words, leaf_type) {
 }
 
 function tokenize(formula) {
+	// ToDo: Fix bug with 'NCFS' token and 'NC' text to parse (missing C)
 	var tokens = [];
 	var branch = prefix_tree;
 	var accum = '';
@@ -22,18 +23,22 @@ function tokenize(formula) {
 		if (char in branch) {
 			accum += char;
 			var parent = branch[char];
-			var {children, leaf_type} = parent;
-			if (leaf_type) [res, res_i] = [accum, i];
+			// var {children, leaf_type} = parent;
 			branch = parent.children;
+			if (parent.leaf_type) {
+				[res, res_i] = [accum, i];
+				continue;
+			};
+			if (i < formula.length - 1) continue;
 		}
-		else {
-			if (!res) throw new Error(`Token ${accum ? accum : char} does not exist!`);
-			tokens.push(res);
-			branch = prefix_tree;
-			accum = '';
-			res = null;
-			i = res_i;
-		}
+		// else {
+		if (!res) throw new Error(`Token ${accum ? accum : char} does not exist!`);
+		tokens.push(res);
+		branch = prefix_tree;
+		accum = '';
+		res = null;
+		i = res_i;
+		// }
 	}
 	tokens.push(res);
 	return tokens;
@@ -46,7 +51,7 @@ function buildBracketTree(text_arr) {
 	var group_obj;
 	for (const item of text_arr) {
 		if (bracket_stack.length) {
-			if (item == ')') bracket_stack.pop();
+			if (item == ')' || item == ']') bracket_stack.pop();
 			if (!bracket_stack.length) {
 				group_obj.brackets.push(item);
 				group_obj.content = buildBracketTree(bracket_content);
@@ -57,11 +62,11 @@ function buildBracketTree(text_arr) {
 			bracket_content.push(item);
 		}
 		else {
-			if (item == '(') group_obj = {brackets: [item], count: null};
+			if (item == '(' || item == '[') group_obj = {brackets: [item], count: null};
 			else if (/^\d+$/.test(item)) bracket_tree.slice(-1)[0].count = parseInt(item);
 			else bracket_tree.push({content: item, brackets: [], count: null});
 		}
-		if (item == '(') bracket_stack.push(item);
+		if (item == '(' || item == '[') bracket_stack.push(item);
 	}
 	return bracket_tree;
 }
@@ -71,7 +76,11 @@ function styleToString(styledict, extras={}) {
 	return Object.entries(aug_styledict).map(entry => entry.join(':')).join(';');
 }
 
-function styleText(text_arr, parent, styledict, [x, y]=[0, 0]) {
+function styleText(text_arr, parent, styledict, [x, y]=[0, 0], center=false) {
+	// Render text_arr
+	// text_arr format:
+	// [str, str, ..., {str: format_flag}, ..., str]
+	// format_flag == 's' - subscript
 	var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 	text.setAttribute('x', x);
 	text.setAttribute('y', y);
@@ -95,28 +104,112 @@ function styleText(text_arr, parent, styledict, [x, y]=[0, 0]) {
 		text.appendChild(tspan);
 		was_dy = is_dy;
 	}
+	if (center) centering(text);
 	return text;
 }
 
 function dockText(anchor, satelite, dir) {
-	var x = parseInt(anchor.getAttribute('x')); 
-	var y = parseInt(anchor.getAttribute('y'));
-	var dy = parseInt(anchor.style.fontSize) * 0.9;
+	// Stack satelite text to the anchor text
+	// if (!satelite.length) return;
+	// console.log('satelite.length', satelite.length, satelite);
 
-	if (dir == 0) x += anchor.getBBox().width;
-	else if (dir == 1) x -= satelite.getBBox().width;
-	else if (dir == 2) y += dy;
-	else if (dir == 3) y -= dy;
+	var x = parseFloat(anchor.getAttribute('x')); 
+	var y = parseFloat(anchor.getAttribute('y'));
+	var dy = parseFloat(anchor.style.fontSize) * 0.9;
+
+	if (dir == 0) x += anchor.getBBox().width; // R
+	else if (dir == 1) x -= satelite.getBBox().width; // L
+	else if (dir == 2) y += dy; // D
+	else if (dir == 3) y -= dy; // U
 
 	satelite.setAttribute('x', x);
 	satelite.setAttribute('y', y);
+}
+
+function attachTextArr(anchor, dir, text_arr, parent, styledict) {
+	if (text_arr.length) {
+		var satelite = styleText(text_arr, parent, styledict);
+		dockText(anchor, satelite, dir);
+		return satelite;
+	}
+}
+
+function flattenBracketTree(bracket_tree, rev=false) {
+	var text_arr=[];
+	for (var {brackets, content, count} of bracket_tree) {
+		var appendix = [];
+		if (brackets.length) appendix.push(brackets[0]);
+		if (content instanceof Array) {
+			var nested_text_arr = flattenBracketTree(content, rev);
+			if (rev) nested_text_arr.reverse();
+			appendix.push(...nested_text_arr)
+		}
+		else appendix.push(content);
+		if (brackets.length) appendix.push(brackets[1]);
+		if (count !== null) appendix.push({[count.toString()]: 's'});
+		if (rev) appendix.reverse();
+		text_arr.push(...appendix);
+	}
+	return text_arr
+}
+
+function firstElemIdx(bracket_tree) {
+	var branch = bracket_tree;
+	var brackets_cnt = 0;
+	var subscript_cnt = 0;
+	while (true) {
+		if (branch[0].brackets.length) brackets_cnt++;
+		if (branch[0].count !== null) subscript_cnt++;
+		if (branch[0].content instanceof Array) branch = branch[0].content;
+		else break;
+	}
+	return [brackets_cnt, subscript_cnt, branch[0].count !== null];
+}
+
+function centering(text) {
+	text.setAttribute('x', parseFloat(text.getAttribute('x')) - text.getBBox().width / 2);
+	text.setAttribute('y', parseFloat(text.getAttribute('y')) + parseFloat(text.style.fontSize) * 0.36);
+	// text.setAttribute('id', 'my_text');
+	// console.log(text);
+	// drawBBox(text);
+	// var {x, y} = text.getStartPositionOfChar(0);
+	// drawPoint(x, y, 0.5);
+	// console.log(x, text.getAttribute('x'));
+}
+
+function textTermBuilder(bracket_tree, parent, dir, styledict, [x, y]) {
+	var rev = dir == 1;
+	var text_arr = flattenBracketTree(bracket_tree, rev);
+	var [brackets_cnt, subscript_cnt, has_1st_subscript] = firstElemIdx(bracket_tree);
+
+	if (dir == 0) {
+		var anch = styleText([text_arr[brackets_cnt]], parent, styledict, [x, y], true);
+		attachTextArr(anch, 1, text_arr.slice(0, brackets_cnt), parent, styledict); // l_sat
+		attachTextArr(anch, 0, text_arr.slice(brackets_cnt + 1), parent, styledict); // r_sat
+	} // R
+	else if (dir == 1) {
+		var both_cnt = brackets_cnt + subscript_cnt;
+		var anch = styleText([text_arr[both_cnt]], parent, styledict, [x, y], true);
+		attachTextArr(anch, 0, text_arr.slice(0, both_cnt).reverse(), parent, styledict); // r_sat
+		attachTextArr(anch, 1, text_arr.slice(both_cnt + 1).reverse(), parent, styledict); // l_sat
+	} // L
+	else if (dir == 2 || dir == 3) {
+		var anch = styleText([text_arr[brackets_cnt]], parent, styledict, [x, y], true);
+		attachTextArr(anch, 1, text_arr.slice(0, brackets_cnt), parent, styledict); // l_sat
+		if (has_1st_subscript) attachTextArr(anch, 0, [text_arr[brackets_cnt + 1]], parent, styledict); // r_sat
+		var vl_brackets = [];
+		var ptr = brackets_cnt + has_1st_subscript;
+		while (['(', '['].includes(text_arr[++ptr])) vl_brackets.push(text_arr[ptr]);
+		var v_sat = attachTextArr(anch, dir, text_arr.slice(ptr), parent, styledict); // v_sat
+		attachTextArr(v_sat, 1, vl_brackets, parent, styledict); // vl_sat
+	} // D, U
 }
 
 
 atoms = new Set(['Mg', 'I', 'Br', 'Cl', 'F', 'S', 'N', 'O', 'C', 'H', 'P', 'B', 'Al', 'NCFS']);
 residues = new Set(['Me', 'Et', 'Pr', 'Bu', 'Ph']);
 punctuation = new Set(['-', ',']);
-brackets = new Set(['(', ')']);
+brackets = new Set(['(', ')', '[', ']']);
 digits = new Set([...Array(10).keys()].map(digit => '' + digit));
 
 styledict = {
@@ -138,16 +231,32 @@ digits.forEach(digit => prefix_tree[digit].children = digit_children);
 
 // console.log(prefix_tree);
 
-var tokens = tokenize('(Br(Mg26(SO4)3(NO3)2)3N5)');
+// var tokens = tokenize('(Br(Mg26(SO4)3(NO3)2)3N5)');
+// var tokens = tokenize('((N2)5CH3)');
+// var tokens = tokenize('N(CH2)5COOH');
+var tokens = tokenize('CHSO3Cl');
+// var tokens = tokenize('(H2(N3))');
 // var tokens = tokenize('Al2(SO4)3');
 
 // var tokens = tokenize('O124N94C3');
 // var tokens = tokenize('NCFI');
 
-// console.log(tokens);
+console.log(tokens);
 
-// var bracket_tree = buildBracketTree(tokens);
-// console.log(bracket_tree);
+var bracket_tree = buildBracketTree(tokens);
+console.log(bracket_tree);
+var flat = flattenBracketTree(bracket_tree, rev=false);
+console.log(flat);
+var [brackets_cnt, subscript_cnt, has_1st_subscript] = firstElemIdx(bracket_tree);
+console.log(brackets_cnt, subscript_cnt, has_1st_subscript);
+
+var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+document.getElementById('utils').appendChild(g);
+
+textTermBuilder(bracket_tree, g, 2, styledict, [180, 150])
+drawPoint(180, 150);
+
+// console.log(JSON.stringify(bracket_tree));
 //`fill:${this.color};font-family:'${this.default_font_family}';font-size:${this.default_size * 0.7}px;`
 
 
@@ -157,9 +266,8 @@ normal_style = styleToString(styledict);
 italic_style = styleToString(styledict, {'font-style': 'italic'});
 // console.log(italic_style);
 
-var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-document.getElementById('utils').appendChild(g);
 
+/*
 var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 text.setAttribute('x', 20);
 text.setAttribute('y', 40);
@@ -185,13 +293,26 @@ drawPoint(char0end.x, char0end.y, 0.5);
 
 
 anch = styleText(['N'], g, styledict, [20, 70]);
-res = styleText(['H', 'S', 'O', {'3': 's'}, {'4': 's'}, 'H'], g, styledict);
+// res = styleText(['H', 'S', 'O', {'3': 's'}, {'4': 's'}, 'H'], g, styledict);
+res = styleText(['HSO', {'34': 's'}, 'H'], g, styledict);
 // res = styleText(['H'], g, styledict);
 
 
-
-
 dockText(anch, res, 0);
+
+
+// styleText(flat.reverse(), g, styledict, [180, 70]);
+styleText(flat, g, styledict, [180, 70]);
+*/
+
+
+
+// var text_arr = buildBracketTree(tokenize('NHSO34H'));
+// console.log(text_arr);
+// console.log(JSON.stringify(text_arr));
+// res2 = styleText(text_arr, g, styledict);
+// dockText(anch, res2, 0);
+
 
 // function dockText(text_arr, parent, anchor, dir) {
 
