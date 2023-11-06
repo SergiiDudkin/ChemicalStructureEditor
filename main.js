f37aaf831b791b16db50ae49be388f06ff6d1d4e
Sergii <sergii@windsor.ai>
2023-11-06 22:26:56 +0200

    Functions to generate icons

--- main.js
+++ main.js
@@ -243,7 +243,7 @@ function toBtnText(text) {
 
 var flex_container = document.getElementsByClassName('flex-container')[0];
 
-var elbtnseq = ['H', 'C', 'O', 'N', 'S', 'F', 'Cl', 'Br', 'I', 'Mg'];
+var elbtnseq = ['C', 'H', 'O', 'N', 'S'];
 
 var movebtn = new RegularButton(flex_container, `
 	<line style="fill:none;stroke:black;stroke-width:2;" x1="15" y1="7" x2="15" y2="23"/>
@@ -253,26 +253,66 @@ var movebtn = new RegularButton(flex_container, `
 	<polygon points="15,27 10.5,22.5 19.5,22.5 "/>
 	<polygon points="15,3 19.5,7.5 10.5,7.5 "/>
 `);
-var dropelbtn = new DropButton(flex_container, toBtnText('A'))
+var dropelbtn = new DropButton(flex_container, `
+	<line x1="15.0" y1="28.0" x2="15.0" y2="19.0" stroke="black" stroke-width="2" />
+	<line x1="2.0" y1="5.5" x2="9.8" y2="10.0" stroke="black" stroke-width="2" />
+	<line x1="28.0" y1="5.5" x2="20.2" y2="10.0" stroke="black" stroke-width="2" />
+	<text x="15" y="15" fill="black" dominant-baseline="middle" text-anchor="middle" font-family="Arial" font-size="16px">A</text>
+`);
 var elbtns = elbtnseq.map(atom => new SubButton(dropelbtn, toBtnText(atom)));
 dropelbtn.focusSubbtn(elbtns[0]);
-var dropbondbtn = new DropButton(flex_container, toBtnText('B'))
-var bondbtn = new SubButton(dropbondbtn, '<text class="but" x="9" y="22" fill="black">&#9585;</text>');
+var dropbondbtn = new DropButton(flex_container, `
+	<line x1="11.0" y1="19.0" x2="19.4" y2="10.6" stroke="black" stroke-width="2" />
+	<text x="6.5" y="24.5" fill="black" dominant-baseline="middle" text-anchor="middle" font-family="Arial" font-size="12px">C</text>
+	<text x="23.5" y="7.5" fill="black" dominant-baseline="middle" text-anchor="middle" font-family="Arial" font-size="12px">C</text>
+`);
+var bondbtn = new SubButton(dropbondbtn, 
+	'<line x1="4.4" y1="25.6" x2="25.6" y2="4.4" stroke="black" stroke-width="2" />'
+);
 dropbondbtn.focusSubbtn(bondbtn);
-var dbondbtn = new SubButton(dropbondbtn, toBtnText('db'));
-var upperbtn = new SubButton(dropbondbtn, toBtnText('up'));
-var lowerbtn = new SubButton(dropbondbtn, toBtnText('dw'));
+var dbondbtn = new SubButton(dropbondbtn, `
+	<line x1="5.8" y1="27.0" x2="27.0" y2="5.8" stroke="black" stroke-width="2" />
+	<line x1="3.0" y1="24.2" x2="24.2" y2="3.0" stroke="black" stroke-width="2" />
+`);
+var upperbtn = new SubButton(dropbondbtn, 
+	'<polygon points="4.7,26.0 27.7,6.5 23.5,2.3 4.0,25.3" fill="black" />'
+);
+var lowerbtn = new SubButton(dropbondbtn, `
+	<defs>
+		<pattern id="low_btn_pattern" x="25.6" y="4.4" width="4" height="1" patternUnits="userSpaceOnUse" patternTransform="rotate(135)">
+			<rect x="0" y="0" width="2" height="1" fill="black" />
+		</pattern>
+	</defs>
+	<polygon points="4.7,26.0 27.7,6.5 23.5,2.3 4.0,25.3" fill="url(#low_btn_pattern)" />
+`);
 var delbtn = new RegularButton(flex_container, `
 	<path style="fill:none;stroke:black;stroke-width:2;" d="M2.5,19.6c-0.7-0.7-0.7-0.7,0-1.4L18.1,2.6c0.7-0.7,0.7-0.7,1.4,0l7.8,7.8c0.7,0.7,0.7,0.7,0,1.4L15.6,23.5c-3.2,3.2-6,3.2-9.2,0L2.5,19.6z"/>
 	<rect x="12.7" y="4.8" transform="matrix(0.7072 0.7071 -0.7071 0.7072 13.2169 -10.3978)" width="13" height="12"/>
 `);
-var textbtn = new RegularButton(flex_container, toBtnText('T'));
-var dropcycbtn = new DropButton(flex_container, toBtnText('Cy'))
-var benzenebtn = new SubButton(dropcycbtn, toBtnText('Ph'));
+var textbtn = new RegularButton(flex_container, `
+	<path d=" M 22 6.8 V 23.2 M 18 5 H 20 A 2 2 0 0 1 22 7 A 2 2 0 0 1 24 5 H 26 M 18 25 H 20 A 2 2 0 0 0 22 23 A 2 2 0 0 0 24 25 H 26" stroke="black" stroke-width="1.5" />
+	<text x="12" y="17.5" fill="black" dominant-baseline="middle" text-anchor="middle" font-family="Serif" font-size="20px">T</text>
+`);
+var dropcycbtn = new DropButton(flex_container, `
+	<polygon points="28.8,15.0 23.6,25.8 11.9,28.5 2.5,21.0 2.5,9.0 11.9,1.5 23.6,4.2" fill="black" />
+	<polygon points="15.0,25.2 5.3,18.2 9.0,6.7 21.0,6.7 24.7,18.2" fill="white" />
+`);
+var benzenebtn = new SubButton(dropcycbtn, `
+	<polygon points="15.0,26.0 5.5,20.5 5.5,9.5 15.0,4.0 24.5,9.5 24.5,20.5" stroke="black" stroke-width="2" fill="none" />
+	<line x1="15.0" y1="22.0" x2="8.9" y2="18.5" stroke="black" stroke-width="2" />
+	<line x1="8.9" y1="11.5" x2="15.0" y2="8.0" stroke="black" stroke-width="2" />
+	<line x1="21.1" y1="11.5" x2="21.1" y2="18.5" stroke="black" stroke-width="2" />
+`);
 dropcycbtn.focusSubbtn(benzenebtn);
-var pentagonbtn = new SubButton(dropcycbtn, toBtnText('5'));
-var hexagonbtn = new SubButton(dropcycbtn, toBtnText('6'));
-var heptagonbtn = new SubButton(dropcycbtn, toBtnText('7'));
+var pentagonbtn = new SubButton(dropcycbtn, 
+	'<polygon points="15.0,24.4 6.1,17.9 9.5,7.4 20.5,7.4 23.9,17.9" stroke="black" stroke-width="2" fill="none" />'
+);
+var hexagonbtn = new SubButton(dropcycbtn, 
+	'<polygon points="15.0,26.0 5.5,20.5 5.5,9.5 15.0,4.0 24.5,9.5 24.5,20.5" stroke="black" stroke-width="2" fill="none" />'
+);
+var heptagonbtn = new SubButton(dropcycbtn,
+	'<polygon points="15.0,27.7 5.1,22.9 2.6,12.2 9.5,3.6 20.5,3.6 27.4,12.2 24.9,22.9" stroke="black" stroke-width="2" fill="none" />'
+);
 
 
 var cnvclip = document.getElementById('cnvclip')
@@ -1021,3 +1061,263 @@ polygonHandler(pentagonbtn, 5);
 polygonHandler(hexagonbtn, 6);
 polygonHandler(heptagonbtn, 7);
 polygonHandler(benzenebtn, 6, true);
+
+
+// Functions to generate icons
+function circPts(num, ctr, vec0) {
+	var rot_angle = Math.PI * 2 / num;
+	var res = [];
+	for (var i = 0; i < num; i++) {
+		res.push(vecSum(ctr, rotateVec(vec0, rot_angle * i)).map(n => n.toFixed(1)));
+	}
+	return res;
+}
+
+function getPvcd(num, bondlength) {
+	var vertex_angle = polygonAngle(num);
+	var pvcd = polygonVertexCtrDist(vertex_angle, bondlength);
+	return pvcd;
+}
+
+function makeIconCycle() {
+	var icon = new RegularButton(flex_container, '');
+
+	var num0 = 7;
+	pts = circPts(num0, [15, 15], [getPvcd(num0, 12), 0]);
+	attachSvg(icon.img, 'polygon', {points: pts.map(item => item.join()).join(' '), fill: 'black'});
+
+	var num1 = 5;
+	pts = circPts(num1, [15, 15], [0, getPvcd(num1, 12)]);
+	attachSvg(icon.img, 'polygon', {points: pts.map(item => item.join()).join(' '), fill: 'white'});
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/polygon/gm, ' /'));
+}
+
+function makeIconCy(num, bondlength) {
+	var icon = new RegularButton(flex_container, '');
+
+	var pts = circPts(num, [15, 15], [0, getPvcd(num, bondlength)]);
+	attachSvg(icon.img, 'polygon', {points: pts.map(item => item.join()).join(' '), stroke: 'black', 'stroke-width': 2, fill: 'none'});
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/polygon/gm, ' /'));
+}
+
+function makeIconPh(bondlength) {
+	var icon = new RegularButton(flex_container, '');
+
+	var num = 6;
+	var pts = circPts(num, [15, 15], [0, getPvcd(num, bondlength)]);
+	attachSvg(icon.img, 'polygon', {points: pts.map(item => item.join()).join(' '), stroke: 'black', 'stroke-width': 2, fill: 'none'});
+
+	pts = circPts(num, [15, 15], [0, getPvcd(num, bondlength - 4)]);
+	for (var i = 0; i < 3; i++) {
+		var [x1, y1] = pts[i * 2];
+		var [x2, y2] = pts[i * 2 + 1];
+		attachSvg(icon.img, 'line', 
+			{x1: x1, y1: y1, x2: x2, y2: y2, stroke: 'black', 'stroke-width': 2}
+		);
+	}
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/polygon/gm, ' /').replaceAll(/\>\<\/line/gm, ' /'));
+}
+
+function makeIconSingleBond(bondlength) {
+	var icon = new RegularButton(flex_container, '');
+
+	var halflen = bondlength / Math.sqrt(2) / 2;
+	var x1 = (15 - halflen).toFixed(1);
+	var y1 = (15 + halflen).toFixed(1);
+	var [x2, y2] = [y1, x1];
+	attachSvg(icon.img, 'line', 
+		{x1: x1, y1: y1, x2: x2, y2: y2, stroke: 'black', 'stroke-width': 2}
+	);
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/line/gm, ' /'));
+}
+
+function makeIconDoubleBond(bondlength, spacing) {
+	var icon = new RegularButton(flex_container, '');
+
+	var halflen = bondlength / Math.sqrt(2) / 2;
+	var pt0 = [15 - halflen, 15 + halflen];
+	var pt1 = pt0.toReversed();
+
+	var uv = unitVec(vecDif(pt0, pt1))
+	var ouva = rot90cw(uv);
+	var shift = vecMul(ouva, spacing);
+
+	var [x1, y1] = vecSum(pt0, shift);
+	var [x2, y2] = vecSum(pt1, shift);
+	attachSvg(icon.img, 'line', 
+		{
+			x1: x1.toFixed(1), 
+			y1: y1.toFixed(1), 
+			x2: x2.toFixed(1), 
+			y2: y2.toFixed(1), 
+			stroke: 'black', 
+			'stroke-width': 2
+		}
+	);
+
+	[x1, y1] = vecDif(shift, pt0);
+	[x2, y2] = vecDif(shift, pt1);
+	attachSvg(icon.img, 'line', 
+		{
+			x1: x1.toFixed(1), 
+			y1: y1.toFixed(1), 
+			x2: x2.toFixed(1), 
+			y2: y2.toFixed(1), 
+			stroke: 'black', 
+			'stroke-width': 2
+		}
+	);
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/line/gm, ' /'));
+}
+
+function makeIconUpperBond(bondlength, spacing0, spacing1) {
+	var icon = new RegularButton(flex_container, '');
+
+	var halflen = bondlength / Math.sqrt(2) / 2;
+	var pt0 = [15 - halflen, 15 + halflen];
+	var pt1 = pt0.toReversed();
+
+	var uv = unitVec(vecDif(pt0, pt1))
+	var ouva = rot90cw(uv);
+	var shift0 = vecMul(ouva, spacing0);
+	var shift1 = vecMul(ouva, spacing1);
+
+	var pts = [
+		vecSum(pt0, shift0).map(val => val.toFixed(1)),
+		vecSum(pt1, shift1).map(val => val.toFixed(1)),
+		vecDif(shift1, pt1).map(val => val.toFixed(1)),
+		vecDif(shift0, pt0).map(val => val.toFixed(1)),
+	];
+
+	attachSvg(icon.img, 'polygon', {points: pts.map(item => item.join()).join(' '), fill: 'black'});
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/polygon/gm, ' /'));
+}
+
+function makeIconLowerBond(bondlength, spacing0, spacing1) {
+	var icon = new RegularButton(flex_container, '');
+
+	var halflen = bondlength / Math.sqrt(2) / 2;
+	var pt0 = [15 - halflen, 15 + halflen];
+	var pt1 = pt0.toReversed();
+
+	var uv = unitVec(vecDif(pt0, pt1))
+	var ouva = rot90cw(uv);
+	var shift0 = vecMul(ouva, spacing0);
+	var shift1 = vecMul(ouva, spacing1);
+
+	var pts = [
+		vecSum(pt0, shift0).map(val => val.toFixed(1)),
+		vecSum(pt1, shift1).map(val => val.toFixed(1)),
+		vecDif(shift1, pt1).map(val => val.toFixed(1)),
+		vecDif(shift0, pt0).map(val => val.toFixed(1)),
+	];
+
+
+	icon.setImage(`
+	<defs>
+		<pattern id="low_btn_pattern" x="${pt1[0].toFixed(1)}" y="${pt1[1].toFixed(1)}" width="4" height="1" patternUnits="userSpaceOnUse" patternTransform="rotate(-45)">
+			<rect x="0" y="0" width="2" height="1" fill="black"></rect>
+		</pattern>
+	</defs>
+	`);
+	attachSvg(icon.img, 'polygon', {points: pts.map(item => item.join()).join(' '), fill: 'url(#low_btn_pattern)'});
+
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/polygon/gm, ' /').replaceAll(/\>\<\/pattern/gm, ' /').replaceAll(/\>\<\/rect/gm, ' /'));
+}
+
+function makeIconBond(bondlength, [b_shift_x, b_shift_y], atomdist, c_y_shift) {
+	var icon = new RegularButton(flex_container, '');
+
+	var halflen = bondlength / Math.sqrt(2) / 2;
+	var x1 = 15 - halflen;// + b_shift_x).toFixed(1);
+	var y1 = 15 + halflen;// + b_shift_y).toFixed(1);
+	var [x2, y2] = [y1, x1];
+	x1 += b_shift_x;
+	y1 += b_shift_y;
+	x2 += b_shift_x;
+	y2 += b_shift_y;
+	[x1, y1, x2, y2] = [x1, y1, x2, y2].map(val => val.toFixed(1))
+
+	attachSvg(icon.img, 'line', 
+		{x1: x1, y1: y1, x2: x2, y2: y2, stroke: 'black', 'stroke-width': 2}
+	);
+
+	var halflen = atomdist / Math.sqrt(2) / 2;
+	var pt0 = [15 - halflen, 15 + halflen];
+	var pt1 = pt0.toReversed();
+
+	var style = {fill: 'black', 'dominant-baseline': 'middle', 'text-anchor': 'middle', 'font-family': 'Arial', 'font-size': '12px'}
+
+	// `<text class='but' x='15' y='17' fill='black' dominant-baseline='middle' text-anchor='middle'>${text}</text>`;
+	var text0 = attachSvg(icon.img, 'text', {x: pt0[0].toFixed(1), y: (pt0[1] + c_y_shift).toFixed(1), ...style});
+	text0.textContent = 'C';
+
+	var text0 = attachSvg(icon.img, 'text', {x: pt1[0].toFixed(1), y: (pt1[1] + c_y_shift).toFixed(1), ...style});
+	text0.textContent = 'C';
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/line/gm, ' /'));
+}
+
+const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
+function makeIconAtom(r_outer, r_inner, ctr) {
+	var icon = new RegularButton(flex_container, '');
+
+	var num = 3;
+	var pts_outer = circPts(num, ctr, [0, r_outer]);
+	var pts_inner = circPts(num, ctr, [0, r_inner]);
+
+	var pts_zip = zip(pts_outer, pts_inner);
+	pts_zip.forEach(([[x1, y1], [x2, y2]]) => attachSvg(icon.img, 'line', 
+		{x1: x1, y1: y1, x2: x2, y2: y2, stroke: 'black', 'stroke-width': 2}
+	));
+
+	var style = {fill: 'black', 'dominant-baseline': 'middle', 'text-anchor': 'middle', 'font-family': 'Arial', 'font-size': '16px'}
+	var text = attachSvg(icon.img, 'text', {x: ctr[0], y: ctr[1] + 2, ...style});
+	text.textContent = 'A';
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/line/gm, ' /'));
+}
+
+function makeIconText(ctr) {
+	var icon = new RegularButton(flex_container, '');
+	var d = `
+		M 22 6.8
+		V 23.2
+		M 18 5
+		H 20
+		A 2 2 0 0 1 22 7
+		A 2 2 0 0 1 24 5
+		H 26
+		M 18 25
+		H 20
+		A 2 2 0 0 0 22 23
+		A 2 2 0 0 0 24 25
+		H 26
+	`
+	attachSvg(icon.img, 'path', {d: d, stroke: 'black', 'stroke-width': 1.5});
+
+	var style = {fill: 'black', 'dominant-baseline': 'middle', 'text-anchor': 'middle', 'font-family': 'Serif', 'font-size': '20px'}
+	var text = attachSvg(icon.img, 'text', {x: ctr[0], y: ctr[1], ...style});
+	text.textContent = 'T';
+
+	console.log(icon.img.innerHTML.replaceAll(/\>\<\/line/gm, ' /').replaceAll(/\>\<\/path/gm, ' /'));
+}
+
+
+// [5, 6, 7].forEach(num => makeIconCy(num, 11));
+// makeIconPh(11);
+// makeIconSingleBond(30);
+// makeIconDoubleBond(30, 2);
+// makeIconUpperBond(30, 0.5, 3);
+// makeIconLowerBond(30, 0.5, 3);
+// makeIconBond(12, [0.2, -0.2], 24, 1);
+// makeIconAtom(15, 6, [15, 13]);
+// makeIconText([12, 17.5]);
