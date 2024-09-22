0dacee259d8877da519f23ef5d2606184d5b517b
Sergii <sergii@windsor.ai>
2024-09-22 02:20:17 +0200

    Debug module refactoring

--- src/scripts/Debug.js
+++ src/scripts/Debug.js
@@ -1,3 +1,8 @@
+import {attachSvg} from './Utils.js';
+
+const DEBUG_GROUP = document.getElementById('debug');
+
+
 function randInt(min_incl, max_excl) {
 	return Math.floor(Math.random() * (max_excl - min_incl) + min_incl);
 }
@@ -9,28 +14,16 @@ export function getColor() {
 
 
 export function drawPoint(x, y, r=1) {
-	var point = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-	point.setAttribute('fill', getColor());
-	point.setAttribute('r', r);
-	point.setAttribute('cx', x);
-	point.setAttribute('cy', y);
-	document.getElementById('debug').appendChild(point);
-}
-
-
-export function clearDebug() {
-	document.getElementById('debug').innerHTML = '';
+	attachSvg(DEBUG_GROUP, 'circle', {fill: getColor(), r: r, cx: x, cy: y});
 }
 
 
 export function drawBBox(text_el) {
-	var text_bbox = text_el.getBBox();
-	var bbox_rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-	bbox_rect.setAttribute('fill', 'blue');
-	bbox_rect.setAttribute('opacity', 0.3);
-	bbox_rect.setAttribute('x', text_bbox.x);
-	bbox_rect.setAttribute('y', text_bbox.y);
-	bbox_rect.setAttribute('width', text_bbox.width);
-	bbox_rect.setAttribute('height', text_bbox.height);
-	document.getElementById('debug').appendChild(bbox_rect);
+	var {x, y, width, height} = text_el.getBBox();
+	attachSvg(DEBUG_GROUP, 'rect', {fill: 'blue', opacity: 0.3, x: x, y: y, width: width, height: height});
+}
+
+
+export function clearDebug() {
+	DEBUG_GROUP.innerHTML = '';
 }
