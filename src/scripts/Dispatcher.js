b0e456852a1f9123671bc8ea8e11ec4339fe61e3
SergiiDudkin <sergii.dudkin@gmail.com>
2025-10-10 03:20:33 +0200

    Isolate dispatcher

---
+++ src/scripts/Dispatcher.js
@@ -0,0 +1,148 @@
+import {MOVE, ROTATE, SCALE, STRETCH, vecMul, checkIntersec} from './Geometry.js';
+import {editStructure} from './Executor.js';
+
+
+const transform_inverts = Object.freeze({
+	[MOVE]: ({moving_vec}) => ({moving_vec: vecMul(moving_vec, -1)}),
+	[ROTATE]: ({rot_angle, rot_ctr}) => ({rot_angle: -rot_angle, rot_ctr: rot_ctr.slice()}),
+	[SCALE]: ({scale_factor, scale_ctr}) => ({scale_factor: 1 / scale_factor, scale_ctr: scale_ctr.slice()}),
+	[STRETCH]: ({stretch_factor, dir_angle, stretch_ctr}) => ({stretch_factor: 1 / stretch_factor,
+		dir_angle: dir_angle + Math.PI, stretch_ctr: stretch_ctr.slice()})
+});
+
+export function invertCmd(kwargs_dir) {
+	let kwargs_rev = {};
+	if (kwargs_dir.del) {
+		kwargs_rev.create = {};
+		for (const [cls_alias, content] of Object.entries(kwargs_dir.del)) {
+			kwargs_rev.create[cls_alias] = gatherData(content);
+		}
+	}
+	if (kwargs_dir.create) {
+		kwargs_rev.del = {};
+		for (const [cls_alias, content] of Object.entries(kwargs_dir.create)) {
+			kwargs_rev.del[cls_alias] = new Set(Object.keys(content));
+		}
+	}
+	if (kwargs_dir.alter) {
+		kwargs_rev.alter = {};
+		for (const [cls_alias, content] of Object.entries(kwargs_dir.alter)) {
+			kwargs_rev.alter[cls_alias] = {};
+			for (const [id, new_attrs] of Object.entries(content)) {
+				kwargs_rev.alter[cls_alias][id] = {};
+				const obj = document.getElementById(id).objref;
+				// eslint-disable-next-line no-unused-vars
+				for (const [key, val] of Object.entries(new_attrs)) {
+					kwargs_rev.alter[cls_alias][id][key] = obj[key];
+				}
+			}
+		}
+	}
+	if (kwargs_dir.transforms) {
+		kwargs_rev.transforms = kwargs_dir.transforms.toReversed().map(([type, ids, params]) => [type,
+			structuredClone(ids), transform_inverts[type](params)]);
+	}
+	return kwargs_rev;
+}
+
+
+class Dispatcher {
+	// eslint-disable-next-line no-unused-vars
+	constructor(executor, inverter, postaction = () => void 0, callback = (cmd, is_undo) => void 0) {
+		this.commands = [];
+		this.ptr = 0;
+		this.executor = executor;
+		this.inverter = inverter;
+		this.postaction = postaction;
+		this.callback = callback;
+		document.addEventListener('keydown', event => this.keyHandler(event)); // !!! ToDo: bind and simplify
+	}
+
+	addCmd(args_dir, args_rev) {
+		this.commands = this.commands.slice(0, this.ptr); // Delete extra commands
+		this.commands.push([args_dir, args_rev]); // Add new command to the history
+		this.ptr++;
+	}
+
+	do(kwargs_dir) {
+		const kwargs_rev = this.inverter(kwargs_dir);
+		this.executor(kwargs_dir);
+		this.addCmd(kwargs_dir, kwargs_rev);
+	}
+
+	redo() {
+		if (this.ptr >= this.commands.length) return;
+		let args_rev = this.commands[this.ptr++][0]; // Fetch command
+		this.executor(args_rev); // Execute the given function with args
+		this.callback(args_rev, false);
+	}
+
+	undo() {
+		if (this.ptr <= 0) return;
+		const args_dir = this.commands[--this.ptr][1]; // Fetch command
+		this.executor(args_dir); // Execute the given function with args
+		this.callback(args_dir, true);
+	}
+
+	keyHandler(event) {
+		if ((event.ctrlKey || event.metaKey) && !event.repeat) {
+			var to_redo = event.key == 'y' || event.key == 'Z' || (event.key == 'z' && event.shiftKey);
+			var to_undo = event.key == 'z' && !event.shiftKey;
+			if (to_redo) this.redo();
+			if (to_undo) this.undo();
+			if (to_undo || to_redo) this.postaction();
+		}
+	}
+}
+
+function detectIntersec(exclude=[]) {
+	var intersections = [];
+	var bond_group = Array.from(document.getElementById('sensors_b').children).map(el => el.objref).filter(bond =>
+		!exclude.includes(bond.id));
+	bond_group.sort((a, b) => a.min_x < b.min_x ? -1 : 1);
+	for (const [i, bond0] of Object.entries(bond_group)) {
+		var j = parseInt(i);
+		var bond1 = bond_group[++j];
+		while (j < bond_group.length && bond1.min_x < bond0.max_x) {
+			if (bond1.min_y < bond0.max_y && bond0.min_y < bond1.max_y &&
+				checkIntersec(...[...bond0.nodes, ...bond1.nodes].map(node => node.xy))
+			) intersections.push([bond0.id, bond1.id].sort((a, b) => parseInt(a.slice(1)) -
+				parseInt(b.slice(1))).join('&'));
+			bond1 = bond_group[++j];
+		}
+	}
+	return intersections;
+}
+
+function getCutouts() {
+	var old_masks = [];
+	for (const mask of [...document.getElementById('bondcutouts').children]) {
+		for (const polygon of [...mask.children]) {
+			if (polygon.tagName != 'polygon') continue;
+			old_masks.push(mask.id.slice(1) + '&' + polygon.classList[0].slice(1));
+		}
+	}
+	return old_masks;
+}
+
+export function refreshBondCutouts(exclude=[]) {
+	var lower_bond, upper_bond;
+	var set_new = new Set(detectIntersec(exclude));
+	var set_old = new Set(getCutouts());
+
+	// Add masks
+	var masks_to_add = [...set_new].filter(new_mask => !set_old.has(new_mask));
+	for (const mask of masks_to_add) {
+		[lower_bond, upper_bond] = mask.split('&').map(id => document.getElementById(id).objref);
+		lower_bond.createSubmask(upper_bond);
+	}
+
+	// Remove masks
+	var masks_to_remove = [...set_old].filter(old_mask => !set_new.has(old_mask));
+	for (const mask of masks_to_remove) {
+		[lower_bond, upper_bond] = mask.split('&').map(id => document.getElementById(id).objref);
+		lower_bond.deleteSubmask(upper_bond);
+	}
+}
+
+export const dispatcher = new Dispatcher(editStructure, invertCmd, refreshBondCutouts);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,7 @@ import {
 	setAttrsSvg, attachSvg, DeletableAbortable, CtrRect, CtrCircle, CtrPolygon, excludeNonExisting} from './Utils.js';
 import {
 	vecLen, findDist, unitVec, vecSum, vecDif, vecMul, vecDotProd, rotateVec, rotateAroundCtr, scaleAroundCtr,
-	stretchAlongDir, polygonAngle, polygonEdgeCtrDist, polygonVertexCtrDist, checkIntersec, MOVE, ROTATE, SCALE, STRETCH
+	stretchAlongDir, polygonAngle, polygonEdgeCtrDist, polygonVertexCtrDist, MOVE, ROTATE, SCALE, STRETCH
 } from './Geometry.js';
 import {ControlPoint} from './ControlPoints.js';
 import {Line, Circle, Rectangle, Polyline, Polygon, Curve, SmoothShape} from './Shapes.js';
@@ -21,9 +21,7 @@ import {
 	pentagonbtn, hexagonbtn, heptagonbtn, arrowbtn, doublearrowbtn, resonancearrowbtn, retroarrowbtn, linebtn, 
 	circlebtn, rectbtn, polylinebtn, polygbtn, curvbtn, smoothbtn
 } from './Buttons.js';
-
-
-window.DEBUG = false;
+import {dispatcher, refreshBondCutouts, invertCmd} from './Dispatcher.js';
 
 
 function downloadSvg() { // Download .svg
@@ -90,107 +88,6 @@ function hideControlPoints() {
 	document.styleSheets[0].cssRules[2].style.opacity = 0;
 }
 
-window.showChessGrid = newcnv.showChessGrid;
-window.hideGrid = newcnv.hideGrid;
-window.showControlPoints = showControlPoints;
-window.hideControlPoints = hideControlPoints;
-
-
-const transform_inverts = Object.freeze({
-	[MOVE]: ({moving_vec}) => ({moving_vec: vecMul(moving_vec, -1)}),
-	[ROTATE]: ({rot_angle, rot_ctr}) => ({rot_angle: -rot_angle, rot_ctr: rot_ctr.slice()}),
-	[SCALE]: ({scale_factor, scale_ctr}) => ({scale_factor: 1 / scale_factor, scale_ctr: scale_ctr.slice()}),
-	[STRETCH]: ({stretch_factor, dir_angle, stretch_ctr}) => ({stretch_factor: 1 / stretch_factor,
-		dir_angle: dir_angle + Math.PI, stretch_ctr: stretch_ctr.slice()})
-});
-
-function invertCmd(kwargs_dir) {
-	let kwargs_rev = {};
-	if (kwargs_dir.del) {
-		kwargs_rev.create = {};
-		for (const [cls_alias, content] of Object.entries(kwargs_dir.del)) {
-			kwargs_rev.create[cls_alias] = gatherData(content);
-		}
-	}
-	if (kwargs_dir.create) {
-		kwargs_rev.del = {};
-		for (const [cls_alias, content] of Object.entries(kwargs_dir.create)) {
-			kwargs_rev.del[cls_alias] = new Set(Object.keys(content));
-		}
-	}
-	if (kwargs_dir.alter) {
-		kwargs_rev.alter = {};
-		for (const [cls_alias, content] of Object.entries(kwargs_dir.alter)) {
-			kwargs_rev.alter[cls_alias] = {};
-			for (const [id, new_attrs] of Object.entries(content)) {
-				kwargs_rev.alter[cls_alias][id] = {};
-				const obj = document.getElementById(id).objref;
-				// eslint-disable-next-line no-unused-vars
-				for (const [key, val] of Object.entries(new_attrs)) {
-					kwargs_rev.alter[cls_alias][id][key] = obj[key];
-				}
-			}
-		}
-	}
-	if (kwargs_dir.transforms) {
-		kwargs_rev.transforms = kwargs_dir.transforms.toReversed().map(([type, ids, params]) => [type,
-			structuredClone(ids), transform_inverts[type](params)]);
-	}
-	return kwargs_rev;
-}
-
-
-class Dispatcher {
-	// eslint-disable-next-line no-unused-vars
-	constructor(executor, inverter, postaction = () => void 0, callback = (cmd, is_undo) => void 0) {
-		this.commands = [];
-		this.ptr = 0;
-		this.executor = executor;
-		this.inverter = inverter;
-		this.postaction = postaction;
-		this.callback = callback;
-		document.addEventListener('keydown', event => this.keyHandler(event)); // !!! ToDo: bind and simplify
-	}
-
-	addCmd(args_dir, args_rev) {
-		this.commands = this.commands.slice(0, this.ptr); // Delete extra commands
-		this.commands.push([args_dir, args_rev]); // Add new command to the history
-		this.ptr++;
-	}
-
-	do(kwargs_dir) {
-		const kwargs_rev = this.inverter(kwargs_dir);
-		this.executor(kwargs_dir);
-		this.addCmd(kwargs_dir, kwargs_rev);
-	}
-
-	redo() {
-		if (this.ptr >= this.commands.length) return;
-		let args_rev = this.commands[this.ptr++][0]; // Fetch command
-		this.executor(args_rev); // Execute the given function with args
-		this.callback(args_rev, false);
-	}
-
-	undo() {
-		if (this.ptr <= 0) return;
-		const args_dir = this.commands[--this.ptr][1]; // Fetch command
-		this.executor(args_dir); // Execute the given function with args
-		this.callback(args_dir, true);
-	}
-
-	keyHandler(event) {
-		if ((event.ctrlKey || event.metaKey) && !event.repeat) {
-			var to_redo = event.key == 'y' || event.key == 'Z' || (event.key == 'z' && event.shiftKey);
-			var to_undo = event.key == 'z' && !event.shiftKey;
-			if (to_redo) this.redo();
-			if (to_undo) this.undo();
-			if (to_undo || to_redo) this.postaction();
-		}
-	}
-}
-
-var dispatcher = new Dispatcher(editStructure, invertCmd, refreshBondCutouts);
-
 
 function getCursorAtom(event, atomtext) {
 	var cursoratom = new ChemNode('cursoratom', ...newcnv.clampEventToCnv(event), '@' + atomtext);
@@ -253,57 +150,6 @@ function iterMolDft(node, atoms=new Set(), bonds=new Set()) {
 		if (!atoms.has(next_node.id)) iterMolDft(next_node, atoms, bonds);
 	}
 	return [atoms, bonds];
-}
-
-
-function detectIntersec(exclude=[]) {
-	var intersections = [];
-	var bond_group = Array.from(document.getElementById('sensors_b').children).map(el => el.objref).filter(bond =>
-		!exclude.includes(bond.id));
-	bond_group.sort((a, b) => a.min_x < b.min_x ? -1 : 1);
-	for (const [i, bond0] of Object.entries(bond_group)) {
-		var j = parseInt(i);
-		var bond1 = bond_group[++j];
-		while (j < bond_group.length && bond1.min_x < bond0.max_x) {
-			if (bond1.min_y < bond0.max_y && bond0.min_y < bond1.max_y &&
-				checkIntersec(...[...bond0.nodes, ...bond1.nodes].map(node => node.xy))
-			) intersections.push([bond0.id, bond1.id].sort((a, b) => parseInt(a.slice(1)) -
-				parseInt(b.slice(1))).join('&'));
-			bond1 = bond_group[++j];
-		}
-	}
-	return intersections;
-}
-
-function getCutouts() {
-	var old_masks = [];
-	for (const mask of [...document.getElementById('bondcutouts').children]) {
-		for (const polygon of [...mask.children]) {
-			if (polygon.tagName != 'polygon') continue;
-			old_masks.push(mask.id.slice(1) + '&' + polygon.classList[0].slice(1));
-		}
-	}
-	return old_masks;
-}
-
-function refreshBondCutouts(exclude=[]) {
-	var lower_bond, upper_bond;
-	var set_new = new Set(detectIntersec(exclude));
-	var set_old = new Set(getCutouts());
-
-	// Add masks
-	var masks_to_add = [...set_new].filter(new_mask => !set_old.has(new_mask));
-	for (const mask of masks_to_add) {
-		[lower_bond, upper_bond] = mask.split('&').map(id => document.getElementById(id).objref);
-		lower_bond.createSubmask(upper_bond);
-	}
-
-	// Remove masks
-	var masks_to_remove = [...set_old].filter(old_mask => !set_new.has(old_mask));
-	for (const mask of masks_to_remove) {
-		[lower_bond, upper_bond] = mask.split('&').map(id => document.getElementById(id).objref);
-		lower_bond.deleteSubmask(upper_bond);
-	}
 }
 
 
@@ -1541,10 +1387,7 @@ class SelectionChem extends SelectionShape {
 	}
 }
 
-
 const selection = new SelectionChem(dispatcher);
-window.selection = selection;
-window.dispatcher = dispatcher;
 
 
 class TransformTool extends DeletableAbortable {
@@ -1832,6 +1675,17 @@ class Indicator extends DeletableAbortable {
 }
 
 
+// Set global variables
+window.DEBUG = false;
+window.showChessGrid = newcnv.showChessGrid;
+window.hideGrid = newcnv.hideGrid;
+window.showControlPoints = showControlPoints;
+window.hideControlPoints = hideControlPoints;
+window.selection = selection;
+window.dispatcher = dispatcher;
+
+
+// Initialize handlers
 for (const elbtn of elbtns) chemNodeHandler(elbtn);
 transformHandler(selrebtn, SelectRect);
 transformHandler(sellabtn, SelectLasso);
