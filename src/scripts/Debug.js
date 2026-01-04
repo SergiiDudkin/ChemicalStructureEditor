91b61ef1a2b772843a545c8d25f54967ff4b2ea1
SergiiDudkin <sergii.dudkin@gmail.com>
2026-01-04 12:11:59 +0100

    Updating Debug.js module

--- src/scripts/Debug.js
+++ src/scripts/Debug.js
@@ -1,6 +1,9 @@
 import {attachSvg} from './Utils.js';
+import {vecDif} from './Geometry.js';
+import {cnv} from './Canvas.js';
 
-const DEBUG_GROUP = document.getElementById('debug');
+
+const debug_group = document.getElementById('debug');
 
 
 function randInt(min_incl, max_excl) {
@@ -14,16 +17,37 @@ export function getColor() {
 
 
 export function drawPoint(x, y, r=1) {
-	attachSvg(DEBUG_GROUP, 'circle', {fill: getColor(), r: r, cx: x, cy: y});
+	attachSvg(debug_group, 'circle', {fill: getColor(), r: r, cx: x, cy: y});
 }
 
 
 export function drawBBox(text_el) {
 	const {x, y, width, height} = text_el.getBBox();
-	attachSvg(DEBUG_GROUP, 'rect', {fill: 'blue', opacity: 0.3, x: x, y: y, width: width, height: height});
+	attachSvg(debug_group, 'rect', {fill: 'blue', opacity: 0.3, x: x, y: y, width: width, height: height});
 }
 
 
 export function clearDebug() {
-	DEBUG_GROUP.innerHTML = '';
+	debug_group.innerHTML = '';
 }
+
+
+function run_debug() {
+	clearDebug();
+	drawPoint(0, 0, 20);
+	drawPoint(800, 800, 20);
+	drawPoint(0, 0, 2);
+	drawPoint(800, 800, 2);
+	console.log('run_debug');
+}
+
+
+function check_dims() {
+	const p0 = cnv.getScreenPoint([0, 0]);
+	const p1 = cnv.getScreenPoint([800, 800]);
+	const dif = vecDif(p0, p1);
+	console.log(`p0 ${p0}, p1 ${p1}, dif ${dif}`)
+}
+
+window.run_debug = run_debug;
+window.check_dims = check_dims;
